b3026d67c3e887d9856df4b7410765bd0662ba27
Abbas0vv <ilhamabbasov46@gmail.com>
2025-03-27 05:22:20 +0400

    Task Halfly Completed

---
+++ ConsoleAppForEmployee/Department.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForEmployee
+{
+    class Department
+    {
+        public Department(string name, int employeeLimit, Employee[] employees)
+        {
+            _name = name;
+            _employeeLimit = employeeLimit;
+            _employees = employees;
+        }
+
+        public string _name { get; set; }
+        public int _employeeLimit { get; set; }
+        public Employee[] _employees { get; set; }
+
+        public void AddEmployee(string name, decimal salary)
+        {
+            Employee employee = new Employee(name, salary);
+        }
+
+        public void GetAllEmployees()
+        {
+            foreach (Employee emp in _employees)
+                Console.WriteLine(emp);
+        }
+    }
+}
---
+++ ConsoleAppForEmployee/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForEmployee
+{
+    class Employee
+    {
+        public Employee(string name, decimal salary)
+        {
+            _name = name;
+            _salary = salary;
+        }
+
+        public string _name { get; set; }
+        public decimal _salary { get; set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Employee : {_name}. Salary : {_salary}");
+        }
+    }
+}
